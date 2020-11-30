8e60802f72e7de96d854a6f3c9c7461df2f513e1
b3an5 <beantheo@gmail.com>
2020-11-30 16:50:22 -0700

    new project

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 1000;
+        int myMinIntVal = Integer.MIN_VALUE;
+        int myMaxIntVal = Integer.MAX_VALUE;
+        System.out.println("int min val = " + myMinIntVal);
+        System.out.println("int max val = " + myMaxIntVal);
+    }
+}
